ca7f81b26d53763dddb6a3bfdb1212032fe51f24
Andrew Almasi <andrewarshia@Andrews-Air-7.attlocal.net>
2023-10-02 23:43:03 -0500

    Made the questions not boring

--- server.js
+++ server.js
@@ -3,8 +3,31 @@ const app = express();
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
+const uri = process.env.MONGODB_URI;
+dbname = "escapeRoom";
+let db;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server    (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db(dbname).command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    db = client.db(dbname)
+    startServer();
+  } catch (error) {
+      console.dir(error);
+  }
+}
+run();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
